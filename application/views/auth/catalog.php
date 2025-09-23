7caed16c02992d8b3b3a72503cae7ed2ef23ea24
maulalardiatqo <maulalardiatqo123@gmail.com>
2025-09-23 22:48:33 +0700

    update catalog database

--- application/views/auth/catalog.php
+++ application/views/auth/catalog.php
@@ -1,40 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-  <header>
-    <h1>Katalog Produk</h1>
-  </header>
-  <main>
-    <div class="catalog" id="catalog"></div>
-  </main>
-
-  <script>
-    const products = <?= json_encode($produk) ?>
-    console.log('product', product)
-    const catalog = document.getElementById("catalog");
-
-    products.forEach(product => {
-      const card = document.createElement("div");
-      card.className = "product-card";
-
-      card.innerHTML = `
-        <img src="<?= base_url('assets/product/' . $product['product_picture'])?>" alt="${product.product_name}" class="product-image">
-        <div class="product-content">
-          <h3 class="product-name">${product.name}</h3>
-          <p class="product-price">${product.price}</p>
-          <p class="product-desc">${product.desc}</p>
-          <button class="btn-order">Pesan</button>
+<?php function decimalToCurrency($value) {
+    return 'Rp. ' . number_format($value, 0, ',', '.');
+}
+?>
+   
+   <section class="padding-large">
+    <div style="height: 30px;">
+            
+          </div>
+    <div class="container">
+        <div class="catalog" id="catalog">
+          
+      <?php foreach ($produk as $p) : ?>
+        <div class="product-card">
+          <img src="<?= base_url('assets/products/' . $p['product_picture']) ?>" alt="$" class="product-image">
+          <div class="product-content">
+            <h3 class="product-name"><?=$p['product_name']?></h3>
+            <p class="product-price"><?= decimalToCurrency($p['price'])?></p>
+            <p class="product-desc"><?= $p['desc']?></p>
+            <button class="btn-order">Pesan</button>
+          </div>
         </div>
-      `;
+       
+        <?php endforeach ?>
+    </div>
+    </div>
+       
+   </section> 
+   
 
-      catalog.appendChild(card);
-    });
-  </script>
-</body>
 
-</html>
+
